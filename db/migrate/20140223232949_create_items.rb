383cdeb29bb98d69624dda30a18571cb76d6dd63
Alexi <forsytaj@miamioh.edu>
2014-02-24 21:40:08 +0000

    Added scaffold item

--- db/migrate/20140223232949_create_items.rb
+++ db/migrate/20140223232949_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :title
-      t.text :description
+      t.text :descrip
       t.integer :price
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221153001) do
+ActiveRecord::Schema.define(version: 20140223232949) do
 
   create_table "items", force: true do |t|
     t.string   "title"
-    t.text     "description"
+    t.text     "descrip"
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
