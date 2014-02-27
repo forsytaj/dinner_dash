40c77f6889d87244fc67300db98eeda78f17a315
Alexi <forsytaj@miamioh.edu>
2014-02-27 01:20:35 +0000

    Fixed issue with create item page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
          
-  validates_presence_of :name
+  validates_presence_of :title
   validates_presence_of :description
   validates_uniqueness_of :title, case_sensitive: false
   validates :price, numericality: {only_integer: true, greater_than: 0}
